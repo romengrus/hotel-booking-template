1163cc74eef503b8efa5affc600437e648fc45ad
Roman Korenev <romengrus@gmail.com>
2019-04-20 19:13:32 +0200

    add svg icons optimization script

---
+++ optimize-icons.js
@@ -0,0 +1,18 @@
+const imagemin = require('imagemin')
+const imageminSvgo = require('imagemin-svgo')
+
+imagemin(['src/assets/icons/**/*.svg'], 'dist/icons', {
+  use: [
+    imageminSvgo({
+      plugins: [
+        { cleanupIDs: { remove: false } },
+        { cleanupNumericValues: { floatPrecision: 2 } },
+        { removeStyleElement: true },
+        { removeTitle: true },
+        { removeViewBox: false },
+        { removeDimensions: true }
+      ],
+      multipass: true
+    })
+  ]
+}).then(() => console.log('SVG-Icons were successfully optimized'))
