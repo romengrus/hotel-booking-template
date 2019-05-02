8a95ca46a52e47b80c8f073eac274d27c91ddfcf
Roman Korenev <romengrus@gmail.com>
2019-05-03 01:25:54 +0200

    refactored form-field-date component

--- src/blocks/library/form-field-date/form-field-date.js
+++ src/blocks/library/form-field-date/form-field-date.js
@@ -1,8 +1,13 @@
+import uuid from 'uuid/v4'
 import IMask from 'imask'
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.form-field-date > input').forEach(el => {
-    IMask(el, {
+class FormFieldDate {
+  constructor($el, app) {
+    this._id = uuid()
+    this.app = app
+    this.$el = $el
+    this.$input = $el.querySelector('.form-field__input')
+    this.mask = IMask(this.$input, {
       mask: Date,
       lazy: false,
       overwrite: true,
@@ -13,5 +18,22 @@ document.addEventListener('DOMContentLoaded', () => {
         Y: { mask: IMask.MaskedRange, placeholderChar: 'Г', from: 1900, to: 2999, maxLength: 4 }
       }
     })
+    this.init()
+  }
+
+  get id() {
+    return this._id
+  }
+
+  init() {
+    // add id to dom component for future reference
+    this.$el.setAttribute('data-component-id', this.id)
+  }
+}
+
+export function factory(app) {
+  const $elements = document.querySelectorAll('.form-field-date')
+  $elements.forEach($el => {
+    app.registerComponent(new FormFieldDate($el, app))
   })
-})
+}
