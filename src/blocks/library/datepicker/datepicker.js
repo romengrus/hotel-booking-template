fead23dbd01704a94827944a2b5738ed48d46729
Roman Korenev <romengrus@gmail.com>
2019-05-03 01:23:28 +0200

    refactored datepicker component

--- src/blocks/library/datepicker/datepicker.js
+++ src/blocks/library/datepicker/datepicker.js
@@ -1,58 +1,88 @@
 import Pikaday from 'pikaday'
+import uuid from 'uuid/v4'
+import datepickerI18n from '../../../i18n/datepicker'
 
-const iconPrev = `<svg class="icon icon-arrow-left"><use xlink:href="#arrow-left"/></svg>`
-const iconNext = `<svg class="icon icon-arrow-right"><use xlink:href="#arrow-right"/></svg>`
+const lang = 'ru'
 
-const i18n = {
-  previousMonth: iconPrev,
-  nextMonth: iconNext,
-  months: [
-    'Январь',
-    'Февраль',
-    'Март',
-    'Апрель',
-    'Май',
-    'Июнь',
-    'Июль',
-    'Август',
-    'Сентябрь',
-    'Октябрь',
-    'Ноябрь',
-    'Декабрь'
-  ],
-  weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-  weekdaysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+class Datepicker {
+  constructor($el, app) {
+    this._id = uuid()
+    this.app = app
+    this.$el = $el
+    const boundedInputId = $el.getAttribute('data-input-id')
+    this.$boundedInput = document.getElementById(boundedInputId)
+    this.$btnReset = $el.querySelector('.datepicker__button-reset')
+    this.$btnOk = $el.querySelector('.datepicker__button-ok')
+    this.picker = this._createPicker()
+    this.init()
+    this.render()
+  }
+
+  get id() {
+    return this._id
+  }
+
+  init() {
+    this._attachEventHandlers()
+
+    // add id to dom component for future reference
+    this.$el.setAttribute('data-component-id', this.id)
+    // set initial date in bounded input
+    if (this.$boundedInput) {
+      this.$boundedInput.value = this.getDate()
+    }
+  }
+
+  render() {
+    const { picker } = this
+    const container = this.$el.querySelector('.datepicker__content')
+    container.appendChild(picker.el)
+  }
+
+  getDate() {
+    return this.picker.toString('DD.MM.YYYY')
+  }
+
+  _createPicker() {
+    const { boundedInputId } = this
+
+    return new Pikaday({
+      i18n: datepickerI18n[lang],
+      field: boundedInputId,
+      firstDay: 1,
+      showDaysInNextAndPreviousMonths: true,
+      defaultDate: new Date(),
+      setDefaultDate: true,
+      onSelect: () => {
+        if (this.$boundedInput) {
+          this.$boundedInput.value = this.getDate()
+        }
+      }
+    })
+  }
+
+  _attachEventHandlers() {
+    this.$btnReset.addEventListener('click', () => this._handleBtnResetClick())
+    this.$btnOk.addEventListener('click', () => this._handleBtnOkClick())
+  }
+
+  _handleBtnResetClick() {
+    this.picker.setDate(new Date())
+    if (this.$boundedInput) {
+      this.$boundedInput.value = this.getDate()
+    }
+  }
+
+  _handleBtnOkClick() {
+    if (this.$boundedInput) {
+      this.$boundedInput.value = this.getDate()
+    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.datepicker').forEach(container => {
-    const inputId = container.getAttribute('data-input-id')
-    const picker = new Pikaday({
-      i18n,
-      field: inputId,
-      firstDay: 1,
-      showDaysInNextAndPreviousMonths: true
-    })
-    const boundedInput = document.querySelector(`#${inputId}`)
-
-    const contentNode = container.querySelector('.datepicker__content')
-    contentNode.appendChild(picker.el)
-
-    // Handle reset button click
-    const btnReset = container.querySelector('.datepicker__btn-reset')
-    btnReset.addEventListener('click', () => {
-      picker.setDate(new Date())
-      if (boundedInput) {
-        boundedInput.value = picker.toString('DD.MM.YYYY')
-      }
-    })
-
-    // Handle ok button click
-    const btnOk = container.querySelector('.datepicker__btn-ok')
-    btnOk.addEventListener('click', () => {
-      if (boundedInput) {
-        boundedInput.value = picker.toString('DD.MM.YYYY')
-      }
-    })
+export function factory(app) {
+  const $datepickers = document.querySelectorAll('.datepicker')
+  $datepickers.forEach($el => {
+    app.registerComponent(new Datepicker($el, app))
   })
-})
+}
