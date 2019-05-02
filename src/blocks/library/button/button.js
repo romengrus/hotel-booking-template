4ad630fc101f0a1e5214876c43382c9cbd37328d
Roman Korenev <romengrus@gmail.com>
2019-05-02 02:43:21 +0200

    refactor button compnent to new format

--- src/blocks/library/button/button.js
+++ src/blocks/library/button/button.js
@@ -1,16 +1,54 @@
-import { startRipple } from '../../effects/ripple/ripple'
+import uuid from 'uuid/v4'
 
-const onButtonClick = e => {
-  const button = e.currentTarget
-  const hasRipple = button.classList.contains('button_has-ripple')
+class Button {
+  constructor($el, app) {
+    this._id = uuid()
+    this.$el = $el
+    this.app = app
+    this.init()
+  }
 
-  if (hasRipple) {
-    startRipple(button, e.clientX, e.clientY)
+  get id() {
+    return this._id
+  }
+
+  init() {
+    this._attachEventHandlers()
+
+    // add id to dom component for future reference
+    this.$el.setAttribute('data-compoent-id', this.id)
+  }
+
+  _attachEventHandlers() {
+    this.$el.addEventListener('click', e => {
+      if (this.$el.classList.contains('button_has-ripple')) {
+        this._showRipple(e)
+      }
+    })
+  }
+
+  _showRipple(e) {
+    const $rippleEl = document.createElement('div')
+    const rect = this.$el.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    // Move ripple block center to mouse click coords
+    $rippleEl.style.top = `${y}px`
+    $rippleEl.style.left = `${x}px`
+
+    this.$el.appendChild($rippleEl)
+    $rippleEl.classList.add('button_ripple-effect')
+
+    window.setTimeout(() => {
+      this.$el.removeChild($rippleEl)
+    }, 1000)
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.button')
-
-  buttons.forEach(btn => btn.addEventListener('click', onButtonClick))
-})
+export function factory(app) {
+  const $buttons = document.querySelectorAll('.button')
+  $buttons.forEach($btn => {
+    app.registerComponent(new Button($btn, app))
+  })
+}
