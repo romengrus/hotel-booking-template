39e97c6c84e12258a7bbf2cf8d51546f6e7e96b2
Roman Korenev <romengrus@gmail.com>
2019-05-03 01:27:17 +0200

    add datepicker localization data

---
+++ src/i18n/datepicker.js
@@ -0,0 +1,22 @@
+export default {
+  ru: {
+    previousMonth: `<svg class="icon icon-arrow-left"><use xlink:href="#arrow-left"/></svg>`,
+    nextMonth: `<svg class="icon icon-arrow-right"><use xlink:href="#arrow-right"/></svg>`,
+    months: [
+      'Январь',
+      'Февраль',
+      'Март',
+      'Апрель',
+      'Май',
+      'Июнь',
+      'Июль',
+      'Август',
+      'Сентябрь',
+      'Октябрь',
+      'Ноябрь',
+      'Декабрь'
+    ],
+    weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    weekdaysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+  }
+}
