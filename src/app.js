555e58975622a605899d8f5540dfc7c45a62e593
Roman Korenev <romengrus@gmail.com>
2019-05-02 02:44:24 +0200

    create app initialization logic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
-import 'blocks/effects/**/*.js'
-import 'blocks/library/**/*.js'
-import 'blocks/project/**/*.js'
+import libraryModules from 'blocks/library/**/*.js'
+import projectComponents from 'blocks/project/**/*.js'
+
+const componentRegistry = {}
+
+class App {
+  constructor(registry) {
+    this._registry = registry
+  }
+
+  registerComponent(component) {
+    this._registry[component.id] = component
+  }
+
+  getComponent(componentId) {
+    return this._registry[componentId]
+  }
+}
+
+const app = new App(componentRegistry)
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Register library components
+  Object.keys(libraryModules).forEach(name => {
+    const block = libraryModules[name]
+    const component = block[name]
+    component.factory(app)
+  })
+
+  // Register project components
+  Object.keys(projectComponents).forEach(name => {
+    const block = libraryModules[name]
+    const component = block[name]
+    component.factory(app)
+  })
+})
