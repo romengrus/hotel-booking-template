7f989371143499f0fca8fab47b7bf5461c26e7dc
Roman Korenev <romengrus@gmail.com>
2019-06-19 00:55:05 +0200

    add room search component

---
+++ src/components/project/form-room-search/__tests__/form-room-search.test.js
@@ -0,0 +1,87 @@
+import path from 'path';
+import pug from 'pug';
+import { FormRoomSearch } from '../form-room-search';
+
+const qs = FormRoomSearch.getQuerySelector();
+const basedir = global.__BASE_DIR;
+const templatePath = path.join(__dirname, 'form-room-search.test.pug');
+const makeComponent = pug.compileFile(templatePath, { basedir });
+
+describe('Form Room Search', () => {
+  test('should render', () => {
+    document.body.innerHTML = makeComponent();
+    const $el = document.querySelector(qs);
+    expect($el).toBeInTheDocument();
+  });
+
+  test('should apply provided modifiers', () => {
+    document.body.innerHTML = makeComponent({ props: { mods: ['modifier1', 'modifier2'] } });
+    const $el = document.querySelector(qs);
+    expect($el).toHaveClass('modifier1', 'modifier2');
+  });
+
+  test('should have form wrapper', () => {
+    document.body.innerHTML = makeComponent();
+    const $el = document.querySelector(qs);
+    const $form = $el.querySelector('.form-room-search__form');
+    expect($form).toBeInTheDocument();
+  });
+
+  test('should apply provided attributes to form wrapper', () => {
+    document.body.innerHTML = makeComponent({ attributes: { foo: 'bar' } });
+    const $el = document.querySelector(qs);
+    const $form = $el.querySelector('.form-room-search__form');
+    expect($form).toHaveAttribute('foo', 'bar');
+  });
+
+  test('should have title with provided text', () => {
+    const labels = { title: 'Test title' };
+    document.body.innerHTML = makeComponent({ props: { labels } });
+    const $el = document.querySelector(qs);
+    const $title = $el.querySelector('.form-room-search__title');
+    expect($title).toBeInTheDocument();
+    expect($title.textContent).toBe(labels.title);
+  });
+
+  test('should have arrival date input', () => {
+    const labels = { arrival: 'Test title' };
+    document.body.innerHTML = makeComponent({ props: { labels } });
+    const $el = document.querySelector(qs);
+    const $input = $el.querySelector('.form-room-search__arrival-input');
+    const $label = $el.querySelector('.form-room-search__arrival-label');
+    expect($input).toBeInTheDocument();
+    expect($label).toBeInTheDocument();
+    expect($label.textContent).toBe(labels.arrival);
+  });
+
+  test('should have departure date input', () => {
+    const labels = { departure: 'Test title' };
+    document.body.innerHTML = makeComponent({ props: { labels } });
+    const $el = document.querySelector(qs);
+    const $input = $el.querySelector('.form-room-search__departure-input');
+    const $label = $el.querySelector('.form-room-search__departure-label');
+    expect($input).toBeInTheDocument();
+    expect($label).toBeInTheDocument();
+    expect($label.textContent).toBe(labels.departure);
+  });
+
+  test('should have guests input', () => {
+    const labels = { guests: 'Test title' };
+    document.body.innerHTML = makeComponent({ props: { labels } });
+    const $el = document.querySelector(qs);
+    const $input = $el.querySelector('.form-room-search__guests-input');
+    const $label = $el.querySelector('.form-room-search__guests-label');
+    expect($input).toBeInTheDocument();
+    expect($label).toBeInTheDocument();
+    expect($label.textContent).toBe(labels.guests);
+  });
+
+  test('should have submit button', () => {
+    const labels = { submit: 'Test title' };
+    document.body.innerHTML = makeComponent({ props: { labels } });
+    const $el = document.querySelector(qs);
+    const $btn = $el.querySelector('.form-room-search__submit');
+    expect($btn).toBeInTheDocument();
+    expect($btn.textContent).toBe(labels.submit);
+  });
+});
--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,2 +1,3 @@
 export * from './subscription/subscription';
 export * from './testimonial/testimonial';
+export * from './form-room-search/form-room-search';
