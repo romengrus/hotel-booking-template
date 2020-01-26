6dcf98cc6b6637cfacfcc489d8e488d11a47a51e
Roman Korenev <romengrus@gmail.com>
2020-01-27 00:18:37 +0200

    Make details page responsive

--- src/components/library/slider/slider.js
+++ src/components/library/slider/slider.js
@@ -3,16 +3,29 @@ import Swiper from 'swiper';
 export class Slider {
   constructor($el) {
     this.$el = $el;
-    this.params = JSON.parse($el.dataset.params);
     this.slider = null;
+    this.thumbs = null;
     this.init();
   }
 
   static getQuerySelector() {
-    return '.swiper-container';
+    return '.slider';
   }
 
   init() {
-    this.slider = new Swiper(this.$el, this.params);
+    const sliderContainer = this.$el.querySelector('.swiper-container');
+    let params = JSON.parse(sliderContainer.dataset.params || {});
+
+    const thumbnailsContainer = this.$el.querySelector('.swiper-container-thumbs');
+
+    if (params.showThumbnails && thumbnailsContainer) {
+      const thumbnailsParams = JSON.parse(thumbnailsContainer.dataset.params || {});
+
+      // connect main slider with thumbs
+      this.thumbs = new Swiper(thumbnailsContainer, thumbnailsParams);
+      params = Object.assign(params, { thumbs: { swiper: this.thumbs } });
+    }
+
+    this.slider = new Swiper(sliderContainer, params);
   }
 }
