55c2345c4399797abc80768cf7551f902a2b354d
Roman Korenev <romengrus@gmail.com>
2020-02-04 20:46:28 +0200

    Use postcss px2rem

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
-    autoprefixer: true
+    autoprefixer: true,
+    'postcss-plugin-px2rem': {
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: [],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: true,
+      minPixelValue: 4
+    }
   }
 };
