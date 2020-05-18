f0ed880d329814bcd4956ef35e9edfe95a4e11ba
Roman Korenev <romengrus@gmail.com>
2020-05-18 16:02:38 +0200

    Refactor button

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 // base components
-export * from './button/button';
 export * from './form/form';
 export * from './form-field-datetime/form-field-datetime';
 export * from './form-field-checkbox/form-field-checkbox';
