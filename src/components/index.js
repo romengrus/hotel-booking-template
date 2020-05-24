cc69f3d53bbcd4a0f596f4d896e475fab486ada1
Roman Korenev <romengrus@gmail.com>
2020-05-24 20:27:58 +0200

    pixel perfect details page

--- src/components/index.js
+++ src/components/index.js
@@ -12,7 +12,6 @@ export * from './doughnut/doughnut';
 
 // compound components
 export * from './subscription/subscription';
-export * from './testimonial/testimonial';
 export * from './room-search/room-search';
 export * from './room-reservation/room-reservation';
 export * from './header/header';
