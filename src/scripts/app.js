a2113ba9b6a75365414e3387085da42455e48bc1
Roman Korenev <romengrus@gmail.com>
2020-06-07 15:19:16 +0200

    fix form-elements pp

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,5 @@
 import '../components/form/form';
 import '../components/datepicker/datepicker';
-import '../components/dropdown-counter/dropdown-counter';
 import '../components/counter/counter';
 import '../components/menu/menu';
 import '../components/off-canvas/off-canvas';
--- src/uikit/form-elements/form-elements.js
+++ src/uikit/form-elements/form-elements.js
@@ -2,5 +2,6 @@ import '../../components/field-mask/field-mask';
 import '../../components/like/like';
 import '../../components/filter-price/filter-price';
 import '../../components/range-slider/range-slider';
+import '../../components/dropdown-counter/dropdown-counter';
 import '../../components/rating/rating';
 import '../../components/panel/panel';
