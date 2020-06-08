f008430bb3addf30ac8351b2b40be07416194e39
Roman Korenev <romengrus@gmail.com>
2020-06-08 10:38:43 +0200

    fix room-details pp

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -1,1 +1,3 @@
 import '../../components/doughnut/doughnut';
+import '../../components/dropdown-counter/dropdown-counter';
+import '../../components/like/like';
--- src/uikit/cards/cards.js
+++ src/uikit/cards/cards.js
@@ -1,3 +1,4 @@
 import '../../components/rating/rating';
 import '../../components/room-card/room-card';
 import '../../components/slider/slider';
+import '../../components/dropdown-counter/dropdown-counter';
