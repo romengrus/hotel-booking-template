37c18c41dd54188dd7c3a3acf7bcf74d5a073b04
Roman Korenev <romengrus@gmail.com>
2020-06-30 16:42:17 +0200

    small issues #4

--- src/uikit/cards/cards.js
+++ src/uikit/cards/cards.js
@@ -1,6 +1,6 @@
 import '../../components/rating/rating';
 import '../../components/room-card/room-card';
 import '../../components/slider/slider';
+import '../../components/counter/counter';
 import '../../components/dropdown-counter/dropdown-counter';
 import '../../components/datepicker/datepicker';
-import '../../components/counter/counter';
